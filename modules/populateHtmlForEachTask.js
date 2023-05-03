f181254c9d39eda4b08440fe2035f5fc154d3d4f
Momo-87 <momochristian87@gmail.com>
2023-05-03 16:25:39 +0200

    modules folder created, code refactored by creating a module for the function used to populate HTML for each task

---
+++ modules/populateHtmlForEachTask.js
@@ -0,0 +1,24 @@
+// A function to iterate over the tasks array and populate an HTML list item element for each task
+const populateHtmlForEachTask = (tasks) => {
+  const toDoListBox = document.querySelector('.to-do-list-box');
+  const taskBox = [];
+  for (let i = 0; i < tasks.length; i += 1) {
+    taskBox[i] = document.createElement('div');
+    taskBox[i].classList.add('flex-row', 'task-box');
+    taskBox[i].contentEditable = true;
+    for (let j = i; j < tasks.length; j += 1) {
+      if (tasks[j].index < tasks[i].index) {
+        // variables swapped in one destructuring assignment
+        [tasks[i], tasks[j]] = [tasks[j], tasks[i]];
+      }
+      taskBox[i].innerHTML = `<div class = "checkBox-and-description-box flex-row">
+                                          <input class = "check-box" type = "checkBox">
+                                          <p class = "task-description">${tasks[i].description}</p>
+                                       </div>
+                                      <div class = "dots-box flex-column"><span class = "dot"></span><span class = "dot"></span><span class = "dot"></span></div>
+                                       `;
+    }
+    toDoListBox.appendChild(taskBox[i]);
+  }
+};
+export default populateHtmlForEachTask;
